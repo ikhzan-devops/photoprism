50ad74fb515a0b71d8262d454d275c2d01ac1826
Keith Martin <keith.martin@outlook.com.au>
2025-02-26 22:29:06 +1000

    Revert to each DBMS having it's own SQL code to avoid unintended consequences

--- internal/entity/query/covers.go
+++ internal/entity/query/covers.go
@@ -25,7 +25,9 @@ func UpdateAlbumDefaultCovers() (err error) {
 
 	condition := gorm.Expr("album_type = ? AND thumb_src = ?", entity.AlbumManual, entity.SrcAuto)
 
-	res = Db().Exec(`UPDATE albums
+	switch DbDialect() {
+	case Postgres:
+		res = Db().Exec(`UPDATE albums
 					SET thumb = (SELECT b.file_hash FROM (SELECT p2.album_uid, f.file_hash FROM files f 
 						INNER JOIN  
 							(SELECT pa.album_uid, max(p.id) AS photo_id FROM photos p
@@ -39,7 +41,29 @@ func UpdateAlbumDefaultCovers() (err error) {
 						AND f.file_type IN (?)
 						) b WHERE b.album_uid = albums.album_uid)
 					WHERE ?`, media.PreviewExpr, condition)
-
+	case MySQL:
+		res = Db().Exec(`UPDATE albums LEFT JOIN (
+						SELECT p2.album_uid, f.file_hash FROM files f, (
+							SELECT pa.album_uid, max(p.id) AS photo_id FROM photos p
+							JOIN photos_albums pa ON pa.photo_uid = p.photo_uid AND pa.hidden = FALSE AND pa.missing = FALSE
+							WHERE p.photo_quality > 0 AND p.photo_private = FALSE AND p.deleted_at IS NULL
+							GROUP BY pa.album_uid) p2 WHERE p2.photo_id = f.photo_id AND f.file_primary = TRUE AND f.file_error = '' AND f.file_type IN (?)
+							) b ON b.album_uid = albums.album_uid
+						SET thumb = b.file_hash WHERE ?`, media.PreviewExpr, condition)
+	case SQLite3:
+		res = Db().Table(entity.Album{}.TableName()).
+			Where("album_type = ? AND thumb_src = ?", entity.AlbumManual, entity.SrcAuto).
+			UpdateColumn("thumb", gorm.Expr(`(
+								SELECT f.file_hash FROM files f 
+									JOIN photos_albums pa ON pa.album_uid = albums.album_uid AND pa.photo_uid = f.photo_uid AND pa.hidden = 0 AND pa.missing = 0
+									JOIN photos p ON p.id = f.photo_id AND p.photo_private = 0 AND p.deleted_at IS NULL AND p.photo_quality > 0
+									WHERE f.deleted_at IS NULL AND f.file_missing = 0 AND f.file_hash <> '' AND f.file_primary = 1 AND f.file_error = '' AND f.file_type IN (?)
+									ORDER BY p.taken_at DESC LIMIT 1
+								)`, media.PreviewExpr))
+	default:
+		log.Warnf("sql: unsupported dialect %s", DbDialect())
+		return nil
+	}
 	err = res.Error
 
 	if err == nil {
@@ -64,19 +88,45 @@ func UpdateAlbumFolderCovers() (err error) {
 
 	condition := gorm.Expr("album_type = ? AND thumb_src = ?", entity.AlbumFolder, entity.SrcAuto)
 
-	res = Db().Exec(`UPDATE albums
-					SET thumb = (SELECT b.file_hash FROM (  SELECT p2.photo_path, f.file_hash FROM files f
-					INNER JOIN
-						(SELECT p.photo_path, max(p.id) AS photo_id FROM photos p   
-							WHERE p.photo_quality > 0 AND p.photo_private = FALSE AND p.deleted_at IS NULL   
-							GROUP BY p.photo_path) p2 
-						ON p2.photo_id = f.photo_id 
-						WHERE f.file_primary = TRUE 
-						AND f.file_error = '' 
-						AND f.file_type IN (?)   
-						) b WHERE b.photo_path = albums.album_path)
-					WHERE ?
-					`, media.PreviewExpr, condition)
+	switch DbDialect() {
+	case Postgres:
+		res = Db().Exec(`UPDATE albums
+						SET thumb = (SELECT b.file_hash FROM (  SELECT p2.photo_path, f.file_hash FROM files f
+						INNER JOIN
+							(SELECT p.photo_path, max(p.id) AS photo_id FROM photos p   
+								WHERE p.photo_quality > 0 AND p.photo_private = FALSE AND p.deleted_at IS NULL   
+								GROUP BY p.photo_path) p2 
+							ON p2.photo_id = f.photo_id 
+							WHERE f.file_primary = TRUE 
+							AND f.file_error = '' 
+							AND f.file_type IN (?)   
+							) b WHERE b.photo_path = albums.album_path)
+						WHERE ?
+						`, media.PreviewExpr, condition)
+	case MySQL:
+		res = Db().Exec(`UPDATE albums LEFT JOIN (
+						SELECT p2.photo_path, f.file_hash FROM files f, (
+							SELECT p.photo_path, max(p.id) AS photo_id FROM photos p
+							WHERE p.photo_quality > 0 AND p.photo_private = FALSE AND p.deleted_at IS NULL
+							GROUP BY p.photo_path) p2 WHERE p2.photo_id = f.photo_id AND f.file_primary = TRUE AND f.file_error = '' AND f.file_type IN (?)
+							) b ON b.photo_path = albums.album_path
+						SET thumb = b.file_hash WHERE ?`, media.PreviewExpr, condition)
+	case SQLite3:
+		res = Db().Table(entity.Album{}.TableName()).
+			Where("album_type = ? AND thumb_src = ?", entity.AlbumFolder, entity.SrcAuto).
+			UpdateColumn("thumb", gorm.Expr(`(
+						SELECT f.file_hash FROM files f,(
+							SELECT p.photo_path, max(p.id) AS photo_id FROM photos p
+							  WHERE p.photo_quality > 0 AND p.photo_private = FALSE AND p.deleted_at IS NULL
+							  GROUP BY p.photo_path
+							) b
+						WHERE f.photo_id = b.photo_id  AND f.file_primary = TRUE AND f.file_error = '' AND f.file_type IN (?)
+						AND b.photo_path = albums.album_path LIMIT 1)
+						`, media.PreviewExpr))
+	default:
+		log.Warnf("sql: unsupported dialect %s", DbDialect())
+		return nil
+	}
 
 	err = res.Error
 
@@ -102,7 +152,9 @@ func UpdateAlbumMonthCovers() (err error) {
 
 	condition := gorm.Expr("album_type = ? AND thumb_src = ?", entity.AlbumMonth, entity.SrcAuto)
 
-	res = Db().Exec(`UPDATE albums
+	switch DbDialect() {
+	case Postgres:
+		res = Db().Exec(`UPDATE albums
 					SET thumb = (SELECT b.file_hash FROM (  SELECT p2.photo_year, p2.photo_month, f.file_hash FROM files f
 					INNER JOIN
 						(SELECT p.photo_year, p.photo_month, max(p.id) AS photo_id FROM photos p   
@@ -114,6 +166,30 @@ func UpdateAlbumMonthCovers() (err error) {
 						AND f.file_type IN (?)   
 						) b WHERE b.photo_year = albums.album_year AND b.photo_month = albums.album_month)
 					WHERE ?`, media.PreviewExpr, condition)
+	case MySQL:
+		res = Db().Exec(`UPDATE albums LEFT JOIN (
+						SELECT p2.photo_year, p2.photo_month, f.file_hash FROM files f, (
+							SELECT p.photo_year, p.photo_month, max(p.id) AS photo_id FROM photos p
+							WHERE p.photo_quality > 0 AND p.photo_private = FALSE AND p.deleted_at IS NULL
+							GROUP BY p.photo_year, p.photo_month) p2 WHERE p2.photo_id = f.photo_id AND f.file_primary = TRUE AND f.file_error = '' AND f.file_type IN (?)
+							) b ON b.photo_year = albums.album_year AND b.photo_month = albums.album_month
+						SET thumb = b.file_hash WHERE ?`, media.PreviewExpr, condition)
+	case SQLite3:
+		res = Db().Table(entity.Album{}.TableName()).
+			Where("album_type = ? AND thumb_src = ?", entity.AlbumMonth, entity.SrcAuto).
+			UpdateColumn("thumb", gorm.Expr(`(
+						SELECT f.file_hash FROM files f,(
+							SELECT p.photo_year, p.photo_month, max(p.id) AS photo_id FROM photos p
+							  WHERE p.photo_quality > 0 AND p.photo_private = FALSE AND p.deleted_at IS NULL
+							  GROUP BY p.photo_year, p.photo_month
+							) b
+						WHERE f.photo_id = b.photo_id AND f.file_primary = TRUE AND f.file_error = '' AND f.file_type IN (?)
+						AND b.photo_year = albums.album_year AND b.photo_month = albums.album_month LIMIT 1)
+						`, media.PreviewExpr))
+	default:
+		log.Warnf("sql: unsupported dialect %s", DbDialect())
+		return nil
+	}
 
 	err = res.Error
 
