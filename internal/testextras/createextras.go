7a61f84f3d7dc2fac059b1c3a28dcf208c6a57f3
Keith Martin <keith.martin@outlook.com.au>
2025-03-07 15:41:13 +1000

    add retry logic as there is a small chance that 2 automigrates happen to run at the same time

--- internal/testextras/createextras.go
+++ internal/testextras/createextras.go
@@ -1,14 +1,37 @@
 package testextras
 
 import (
+	"time"
+
 	"gorm.io/gorm"
 )
 
 func MigrateTestExtras(db *gorm.DB) {
-	if err := db.AutoMigrate(&TestLog{}); err != nil {
+	var err error
+	for migrateRetry := 0; migrateRetry < 10; migrateRetry++ {
+		if err = db.AutoMigrate(&TestLog{}); err != nil {
+			log.Warnf("migratetestextras: automigrate testlog encountered %+v on loop %s", err, migrateRetry)
+			if migrateRetry < 10 {
+				time.Sleep(time.Second * 5)
+			}
+		} else {
+			break
+		}
+	}
+	if err != nil {
 		panic(err)
 	}
-	if err := db.AutoMigrate(&TestDBMutex{}); err != nil {
+	for migrateRetry := 0; migrateRetry < 10; migrateRetry++ {
+		if err = db.AutoMigrate(&TestDBMutex{}); err != nil {
+			log.Warnf("migratetestextras: automigrate testdbmutex encountered %+v on loop %s", err, migrateRetry)
+			if migrateRetry < 10 {
+				time.Sleep(time.Second * 5)
+			}
+		} else {
+			break
+		}
+	}
+	if err != nil {
 		panic(err)
 	}
 }
