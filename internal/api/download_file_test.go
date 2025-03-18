8c4bd1f3ed763e78ae9d2ce705625a24201be757
Michael Mayer <michael@photoprism.app>
2025-03-18 14:48:06 +0100

    API: Add unit tests for DownloadName() and AlbumDownloadName() #4344
    Signed-off-by: Michael Mayer <michael@photoprism.app>

--- internal/api/download_file_test.go
+++ internal/api/download_file_test.go
@@ -4,11 +4,26 @@ import (
 	"net/http"
 	"testing"
 
+	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 	"github.com/tidwall/gjson"
 
 	"github.com/photoprism/photoprism/internal/config"
+	"github.com/photoprism/photoprism/internal/config/customize"
 )
+
+func TestDownloadName(t *testing.T) {
+	t.Run("File", func(t *testing.T) {
+		req, err := http.NewRequest(http.MethodGet, "api/v1/dl?name=file", nil)
+		assert.NoError(t, err)
+
+		c := &gin.Context{
+			Request: req,
+		}
+
+		assert.Equal(t, customize.DownloadNameFile, DownloadName(c))
+	})
+}
 
 func TestGetDownload(t *testing.T) {
 	t.Run("NotFound", func(t *testing.T) {
