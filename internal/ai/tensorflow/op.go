7bcbaea4926c47af199f264f03d73061263a7c11
raystlin <raystlin@gmail.com>
2025-04-11 14:32:17 +0000

    Dynamically add a softmax layer to a loaded graph.
    By inspecting existing models we saw that many times logits are returned
    instead of probabilities. Photoprism uses probabilites to rank the
    quality of the results, so we need to transform those logits. Our
    approach is to add a new layer at runtime to the graph that performs the
    softmax operation.

---
+++ internal/ai/tensorflow/op.go
@@ -0,0 +1,45 @@
+package tensorflow
+
+import (
+	tf "github.com/wamuir/graft/tensorflow"
+)
+
+func AddSoftmax(graph *tf.Graph, info *ModelInfo) (*tf.Operation, error) {
+
+	randomName := randomString(10)
+
+	logits := graph.Operation(info.Output.Name).Output(info.Output.OutputIndex)
+	reshapeOpSpec := tf.OpSpec{
+		Type: "EnsureShape",
+		Name: randomString(10),
+		Input: []tf.Input{
+			logits,
+		},
+		Attrs: map[string]any{
+			"shape": tf.MakeShape(-1, info.Output.NumOutputs),
+		},
+	}
+
+	reshapeOp, err := graph.AddOperation(reshapeOpSpec)
+	if err != nil {
+		return nil, err
+	}
+
+	opspec := tf.OpSpec{
+		Type: "Softmax",
+		Name: randomName,
+		Input: []tf.Input{
+			reshapeOp.Output(0),
+		},
+	}
+
+	op, err := graph.AddOperation(opspec)
+	if err != nil {
+		return nil, err
+	}
+
+	info.Output.Name = randomName
+	info.Output.OutputIndex = 0
+
+	return op, nil
+}
