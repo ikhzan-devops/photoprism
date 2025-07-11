aca000854cbc9d76cc2c9227df1c4f420072b152
Michael Mayer <michael@photoprism.app>
2025-07-11 11:59:46 +0200

    Scripts: Add doctl CLI command to install-admin-tools.sh
    Signed-off-by: Michael Mayer <michael@photoprism.app>

--- scripts/dist/install-admin-tools.sh
+++ scripts/dist/install-admin-tools.sh
@@ -18,14 +18,17 @@ then
     exit 1
 fi
 
-echo "Installing duf, a better df alternative..."
+echo "Installing the duf command to check storage usage..."
 GOBIN="/usr/local/bin" go install github.com/muesli/duf@latest
 
-echo "Installing muffet, a fast website link checker..."
+echo "Installing muffet, a tool for checking links..."
 GOBIN="/usr/local/bin" go install github.com/raviqqe/muffet@latest
 
-echo "Installing petname, an RFC1178 implementation to generate pronounceable names..."
+echo "Installing petname to generate pronounceable names..."
 GOBIN="/usr/local/bin" go install github.com/dustinkirkland/golang-petname/cmd/petname@latest
+
+echo "Installing doctl for using the DigitalOcean API...."
+GOBIN="/usr/local/bin" go install github.com/digitalocean/doctl/cmd/doctl@latest
 
 # Create a symbolic link for "duf" so that it is used instead of the original "df".
 ln -sf /usr/local/bin/duf /usr/local/bin/df
