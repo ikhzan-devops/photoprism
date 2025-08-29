2f5cc62d45f8d5eb06d8c296db4f1647d3d0391d
Jarrad Whitaker <akdor1154@gmail.com>
2025-08-29 22:44:32 +1000

    rename adobe rgb icc profile

--- internal/thumb/icc.go
+++ internal/thumb/icc.go
@@ -12,7 +12,7 @@ up to `assets`, so I'm punting on that for now.
 */
 
 func MustGetAdobeRGB1998Path() string {
-	p := path.Join(IccProfilesPath, "compatibleWithAdobeRGB1998.icc")
+	p := path.Join(IccProfilesPath, "adobe_rgb_compat.icc")
 	_, err := os.Stat(p)
 	if err != nil {
 		panic(err)
