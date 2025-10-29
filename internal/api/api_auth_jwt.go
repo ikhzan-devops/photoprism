630506e0213cf16d95bc27a600280123929ea57c
Michael Mayer <michael@photoprism.app>
2025-10-29 15:21:49 +0100

    Auth: Remove redundant preview/download token wiring for JWT #5230
    Signed-off-by: Michael Mayer <michael@photoprism.app>

--- internal/api/api_auth_jwt.go
+++ internal/api/api_auth_jwt.go
@@ -112,18 +112,6 @@ func authAnyJWT(c *gin.Context, clientIP, authToken string, resource acl.Resourc
 		IssuedAt:  issuedAt,
 		NotBefore: notBefore,
 		ExpiresAt: expiresAt,
-		PreviewToken: func() string {
-			if tokenScopes.Contains(acl.ResourceFiles.String()) {
-				return conf.PreviewToken()
-			}
-			return ""
-		}(),
-		DownloadToken: func() string {
-			if tokenScopes.Contains(acl.ResourceFiles.String()) {
-				return conf.DownloadToken()
-			}
-			return ""
-		}(),
 	})
 }
 
