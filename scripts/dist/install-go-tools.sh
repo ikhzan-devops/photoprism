8fc9f807de8ae9080a93431bf4ae0b9814265fee
Michael Mayer <michael@photoprism.app>
2025-11-21 08:56:25 +0100

    Develop: Add .golangci.yml and update install-go-tools.sh script
    Signed-off-by: Michael Mayer <michael@photoprism.app>

--- scripts/dist/install-go-tools.sh
+++ scripts/dist/install-go-tools.sh
@@ -44,6 +44,7 @@ case $DESTARCH in
     GOBIN="/usr/local/bin" go install github.com/psampaz/go-mod-outdated@latest
     GOBIN="/usr/local/bin" go install github.com/kyoh86/richgo@latest
     GOBIN="/usr/local/bin" go install github.com/goreleaser/nfpm/v2/cmd/nfpm@latest
+    GOBIN="/usr/local/bin" go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
     ;;
 
   *)
@@ -58,6 +59,7 @@ case $DESTARCH in
     GOBIN="/usr/local/bin" go install github.com/goreleaser/nfpm/v2/cmd/nfpm@latest
     GOBIN="/usr/local/bin" go install github.com/google/go-licenses@latest
     GOBIN="/usr/local/bin" go install github.com/swaggo/swag/cmd/swag@latest
+    GOBIN="/usr/local/bin" go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
     ;;
 esac
 
